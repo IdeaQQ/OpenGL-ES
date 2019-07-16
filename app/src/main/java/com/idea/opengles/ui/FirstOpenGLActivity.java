703bc2d810b4cea2bfa0fb0d8c3dd277261e41f9
qinjijie <qinjijie@unisound.com>
2019-07-16 17:11:27 +0800

    feat : first commit

---
+++ app/src/main/java/com/idea/opengles/ui/FirstOpenGLActivity.java
@@ -0,0 +1,55 @@
+package com.idea.opengles.ui;
+
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+import com.idea.opengles.render.FirstOpenGLESRender;
+
+public class FirstOpenGLActivity extends Activity {
+
+    private GLSurfaceView mGLSurfaceView;
+    private boolean mRenderSet = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mGLSurfaceView = new GLSurfaceView(this);
+        setContentView(mGLSurfaceView);
+        initOpenGles();
+    }
+
+    private void initOpenGles() {
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportGLEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+        if (supportGLEs2) {
+            mGLSurfaceView.setEGLContextClientVersion(2);
+            mGLSurfaceView.setRenderer(new FirstOpenGLESRender());
+            mRenderSet = true;
+        } else {
+            return;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mRenderSet) {
+            mGLSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mRenderSet) {
+            mGLSurfaceView.onPause();
+        }
+    }
+
+}
